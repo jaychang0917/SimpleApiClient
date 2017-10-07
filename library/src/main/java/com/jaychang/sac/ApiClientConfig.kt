cff23474f595adc534e93185aee7f7870ad60da0
jay <jaychang0917@gmail.com>
2017-10-07 19:59:30 +0800

    Clean code

--- library/src/main/java/com/jaychang/sac/ApiClientConfig.kt
+++ library/src/main/java/com/jaychang/sac/ApiClientConfig.kt
@@ -1,15 +1,16 @@
 package com.jaychang.sac
 
 import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
 import kotlin.reflect.KClass
 
 typealias LogLevel = HttpLoggingInterceptor.Level
 
 data class ApiClientConfig(
   var baseUrl: String = "",
-  var connectTimeout: Long = 10 * 6000,
-  var readTimeout: Long = 10 * 6000,
-  var writeTimeout: Long = 10 * 6000,
+  var connectTimeout: Long = TimeUnit.MINUTES.toMillis(1),
+  var readTimeout: Long = TimeUnit.MINUTES.toMillis(1),
+  var writeTimeout: Long = TimeUnit.MINUTES.toMillis(1),
   var defaultHeaders: Map<String, String>? = null,
   var defaultParameters: Map<String, String>? = null,
   var errorClass: KClass<*>? = null,
