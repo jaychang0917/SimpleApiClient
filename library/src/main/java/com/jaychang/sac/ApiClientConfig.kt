dd8e9b858bfd5bb1b17e697ce4b7d76ac128163d
jay <jaychang0917@gmail.com>
2017-10-08 15:59:29 +0800

    Clean code

--- library/src/main/java/com/jaychang/sac/ApiClientConfig.kt
+++ library/src/main/java/com/jaychang/sac/ApiClientConfig.kt
@@ -2,7 +2,6 @@ package com.jaychang.sac
 
 import okhttp3.logging.HttpLoggingInterceptor
 import java.util.concurrent.TimeUnit
-import kotlin.reflect.KClass
 
 typealias LogLevel = HttpLoggingInterceptor.Level
 
@@ -13,8 +12,6 @@ data class ApiClientConfig(
   var writeTimeout: Long = TimeUnit.MINUTES.toMillis(1),
   var defaultHeaders: Map<String, String>? = null,
   var defaultParameters: Map<String, String>? = null,
-  var errorClass: KClass<*>? = null,
-  var apiClass: KClass<*>? = null,
   var errorHandler: ((Throwable) -> Unit)? = null,
   var certificatePins: List<CertificatePin>? = null,
   var isStethoEnabled: Boolean = true,
